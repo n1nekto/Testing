7160a27c72d6e9339a51eb504a4a1ccaa7d3987a
n1nekto <ad.moxito@mail.ru>
2022-04-13 09:33:37 +0300

    Обновил main и test_main. Финальный вариант

--- main.py
+++ main.py
@@ -6,15 +6,16 @@ class Product:
         self.name, self.price = name, price
 
 class Basket:
-    basket: List[Product] = []
+    def __init__(self, products=[]):
+        self.basket: List[Product] = products
+
+    # basket: List[Product] = []
 
     def add(self, product):
         self.basket.append(product)
-        return 'add'
 
     def remove(self, product):
         self.basket.remove(product)
-        return 'remove'
 
     def count_sum(self):
         return sum([pr.price for pr in self.basket])
--- test_main.py
+++ test_main.py
@@ -21,19 +21,27 @@ def test_price():
     product = Product('name', 10)
     assert product.price == 10
 
+a = Product('a', 50)
+b = Product('b', 30)
 
 def test_add():
-    basket_1 = Basket()
-    basket_1.add(Product('b', 30))
-    assert 'add' == basket_1.add(Product('b', 30))
+    basket3 = Basket([a])
+    basket4 = Basket([a, b])
+    basket3.add(b)
+    assert basket3.basket == basket4.basket
 
+    # pr.name for pr in self.basket
+def test_remove():
+    basket = Basket([a, b])
+    basket2 = Basket([a])
+    basket.remove(b)
+    assert basket2.basket == basket.basket
 
-test_2 = [([Product('zxc', 993), Product('qwe', 7)], 'remove')]
-
-@pytest.mark.parametrize('products, s', test_2)
-def test_remove_basket(products, s):
-    basket_2 = Basket()
-    for p in products:
-        basket_2.add(p)
-        assert s == basket_2.remove(p)
-
+# test_2 = [([Product('zxc', 993), Product('qwe', 7)], 'remove')]
+#
+# @pytest.mark.parametrize('products, s', test_2)
+# def test_remove_basket(products, s):
+#     basket_2 = Basket()
+#     for p in products:
+#         basket_2.add(p)
+#         assert s == basket_2.remove(p)
